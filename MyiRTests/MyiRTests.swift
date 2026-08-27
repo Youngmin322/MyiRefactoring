e4e3f2974d2295f97c29983205ab5b8eaeb375f2
Youngmin Cho <choyoungmin0322@gmail.com>
2026-08-27 23:47:45 +0900

    chore: 테스트 타깃 및 배포 설정 추가
    - Unit Test 타깃 추가
    - 최소 지원 버전을 iOS 18.6으로 설정

---
+++ MyiRTests/MyiRTests.swift
@@ -0,0 +1,18 @@
+//
+//  MyiRTests.swift
+//  MyiRTests
+//
+//  Created by Youngmin Cho on 8/27/26.
+//
+
+import Testing
+
+struct MyiRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
