d4a8be59777075538e107487ee6dc8f1d9b1309b
Youngmin Cho <choyoungmin0322@gmail.com>
2026-08-27 22:28:41 +0900

    Initial Commit

---
+++ MyiR/MyiRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyiRApp.swift
+//  MyiR
+//
+//  Created by Youngmin Cho on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MyiRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
